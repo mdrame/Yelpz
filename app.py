1014989f83dd1d180aaf55282cbe5c2e20ce4b03
mohammed <mdrame113@gmail.com>
2019-10-12 23:49:14 -0700

    API Call succeded

--- app.py
+++ app.py
@@ -4,18 +4,20 @@ from random import sample
 import requests
 import json
 import os #creating a enviromental variable : so our api key and id won't get pushed to github or remotely.
-
+# from YelpAPI import get_my_key
 
 app = Flask(__name__)
 
 messages = ["Wow you is beautiful", "Awesome", "Lets get marry", "Precious", "Your are so awesome"]
 
 # Yelp business info API request url
-# https://api.yelp.com/v3/businesses/{id}
+business_id = 'adLzMuVhL1CSj0j0VeXTZQ'
 
-id = 'adLzMuVhL1CSj0j0VeXTZQ'
-key = 'P8HZvWFwVupqzc7RTaLgjHxRIN0f6E380U6ZpIFCJaPZl-ButUiJlrTq89KVnDkRuesCGBinXj8MPqiUL_KT9ooFU1xHsDe72NBVDWO_MhmKDKpOgUHOASvVG6GiXXYx'
 
+API_KEY = 'P8HZvWFwVupqzc7RTaLgjHxRIN0f6E380U6ZpIFCJaPZl-ButUiJlrTq89KVnDkRuesCGBinXj8MPqiUL_KT9ooFU1xHsDe72NBVDWO_MhmKDKpOgUHOASvVG6GiXXYx'
+ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
+# key = 'P8HZvWFwVupqzc7RTaLgjHxRIN0f6E380U6ZpIFCJaPZl-ButUiJlrTq89KVnDkRuesCGBinXj8MPqiUL_KT9ooFU1xHsDe72NBVDWO_MhmKDKpOgUHOASvVG6GiXXYx'
+HEADERS = {"Authorization": "bearer %s" % API_KEY}
 
 #home route 
 @app.route('/')
@@ -29,13 +31,18 @@ def home():
 @app.route("/showResult")
 def showResult():
 
-    search = request.args.get("storeName")
+    search = request.args.get("term")
     # this is usually right behind the url of API address
-    params = {  "term": search,
-                "key": key
+    PARAMETERS = {  "term": search,
+                    "location": "San Diego"
+                # "key": key
                 }
 
-    r = requests.get("https://api.yelp.com/v3/businesses/search",id=params)
+#https://api.yelp.com/v3/businesses/search
+    r = requests.get(url = ENDPOINT,
+                    params=PARAMETERS, 
+                    headers=HEADERS)
+
     jokes_json = r.json()
     # print(jokes_json)
     #three_messages = sample(messages, 3)
