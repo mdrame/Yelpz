daec629d2efd7539000c5c8555c70de1a9860d51
mohammed <mdrame113@gmail.com>
2019-10-15 11:28:29 -0700

    get data successfully from yelp

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import requests
 import json
 import os #creating a enviromental variable : so our api key and id won't get pushed to github or remotely.
 # from YelpAPI import get_my_key
+import pprint #pyhton json viewer  libary
 
 app = Flask(__name__)
 
@@ -34,20 +35,20 @@ def showResult():
     search = request.args.get("term")
     # this is usually right behind the url of API address
     PARAMETERS = {  "term": search,
-                    "location": "San Diego"
+                    "location": "San Fancisco"
                 # "key": key
                 }
 
-#https://api.yelp.com/v3/businesses/search
     r = requests.get(url = ENDPOINT,
-                    params=PARAMETERS, 
+                    params=PARAMETERS,
                     headers=HEADERS)
 
-    jokes_json = r.json()
-    # print(jokes_json)
-    #three_messages = sample(messages, 3)
+    business_json = r.json()
+    # pp = pprint.PrettyPrinter(indent=4) #indexting by four 
+    # pp.pprint(business_json) #print data from yelp
+    # #three_messages = sample(messages, 3)
 
-    return f"user entered: {search} {jokes_json}"
+    return render_template("show_Business.html", business_json=business_json)
 
 
 
