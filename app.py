a01ef8320da3f0b58a14089d15e1512437df233f
mohammed <mdrame113@gmail.com>
2019-10-16 15:53:04 -0700

    Geo location added to City field using user AIP

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ import pprint #pyhton json viewer  libary
 
 app = Flask(__name__)
 
+def get_country(ip_address):
+    try:
+        response = requests.get("http://ip-api.com/json/{}".format(ip_address))
+        js = response.json()
+        city = js['city']
+        return city
+    except Exception as e:
+        return "Error : We cant seems to find your location, pls turn on your connectivity"
+
 stars = {"oneStar": "⭐️",
          "twoStar": "⭐️ ⭐️",
          "threeStar": "⭐️ ⭐️ ⭐️",
@@ -28,9 +37,17 @@ HEADERS = {"Authorization": "bearer %s" % API_KEY}
 @app.route('/')
 def home():
    
+    ip_address = request.remote_addr
+    city = get_country(ip_address)
+    # number of countries where the largest number of speakers are French
+    # data from http://download.geonames.org/export/dump/countryInfo.txt
+    # if country in ('BL', 'MF', 'TF', 'BF', 'BI', 'BJ', 'CD', 'CF', 'CG', 'CI', 'DJ', 'FR', 'GA', 'GF', 'GN', 'GP', 'MC', 'MG', 'ML', 'MQ', 'NC'):
+    #     return "Bonjour"
+        #testing
+    # return "Hello"
 
   
-   return render_template("index.html")
+    return render_template("index.html", city=city)
 
 # show result base on what user enter in seach box
 @app.route("/showResult")
@@ -48,10 +65,17 @@ def showResult():
                     params=PARAMETERS,
                     headers=HEADERS)
 
+    
+
     business_json = r.json()
     # pp = pprint.PrettyPrinter(indent=4) #indexting by four 
     # pp.pprint(business_json) #print data from yelp
     # #three_messages = sample(messages, 3)
+
+    #error handing to let user know that there is no match
+    # if not business_json:
+    #     no_business_found = " Sorry No bussiness match you search "
+    #     return
 
     return render_template("show_Business.html", stars=stars,  business_json=business_json)
 
